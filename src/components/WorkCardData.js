d90dd333247d7d65b09c5fc123c92026df504321
niyatijain1 <118036643+niyatijain1@users.noreply.github.com>
2023-08-14 01:54:41 +0530

    complete

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,23 @@
+import pro1 from "../assets/project1img.png"
+import pro2 from "../assets/project2img.png"
+
+
+const ProjectCardData = [
+
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Web application",
+        text: "Technologies used - HTML, Tailwind CSS, React Js",
+        view: "https://ecommercewebappniyatijain.netlify.app/"
+
+    },
+    {
+        imgsrc: pro2,
+        title: "Niyati-fitnesss website",
+        text: "Techonologies used - HTML, CSS, Javascript",
+        view: "http://niyatifitnesss.000webhostapp.com/"
+
+    },
+]
+
+export default ProjectCardData;
